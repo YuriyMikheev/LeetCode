2eb8b0600f9ec84033455c055db064de51150bf5
YuriyMikheev <yuri.mikheev@gmail.com>
2022-04-04 14:07:07 +0300

    Roman To  Int

--- src/yuriyleetcode/src/main/java/Solution.java
+++ src/yuriyleetcode/src/main/java/Solution.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 public class Solution {
 
 
+    /**
+     * Roman to Integer problem @see <a href="https://leetcode.com/problems/roman-to-integer/"> https://leetcode.com/problems/roman-to-integer </a>
+     *
+     * @param s
+     * @return
+     */
 
     public static int romanToInt(String s) {
 
@@ -32,6 +40,95 @@ public class Solution {
 
         return r;
     }
+
+
+    /**
+     * 3sum problem @see <a href="https://leetcode.com/problems/3sum/"> https://leetcode.com/problems/3sum </a>
+     *
+     *
+     * The solutions complexity is O(n^2) in case of all different input numbers and
+     * in case of repeated numbers the complexity could be much lower O(n + unique(nums)^2)
+     *
+     * @param nums
+     * @return
+     */
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        HashSet<Integer> hs;
+        HashMap<Integer, HashSet<Integer>> g = new HashMap<>();
+        int j = 0;
+        for (int i: nums) {
+            if (g.containsKey(i)) hs = g.get(i);
+            else g.put(i, hs = new HashSet<>());
+            hs.add(j++);
+        }
+        LinkedList<Integer> integers = new LinkedList<>();
+
+        for (Map.Entry<Integer, HashSet<Integer>> entry: g.entrySet()){
+            for (int i = 0; i < Math.min(3, entry.getValue().size()); i++)
+                integers.add(entry.getKey());
+        }
+
+        Integer[] numbs = integers.toArray(new Integer[0]);
+
+        Arrays.sort(numbs);
+
+        g.clear(); j = 0;
+
+        for (int i: numbs) {
+            if (g.containsKey(i)) hs = g.get(i);
+            else g.put(i, hs = new HashSet<>());
+            hs.add(j++);
+        }
+
+
+        HashMap<String, int[]> found = new HashMap<>();
+
+        for (int i = 0; i < numbs.length; i++){
+            for (int k = numbs.length - 1; k >= i; --k){
+                int r = numbs[i] + numbs[k];
+                int rn = 0;
+                if (-r == numbs[i]) rn++;
+                if (-r == numbs[k]) rn++;
+                {
+
+                    if (g.containsKey(-r))
+                        if ((hs = g.get(-r)).size() > rn)
+
+                        {
+                            Iterator<Integer> iterator = hs.iterator();
+                            int c = iterator.next();
+
+                            while (iterator.hasNext() & c == i & c == k)
+                                c = iterator.next();
+                            if (c != i & c != k & i!=k) {
+                                int[] r1 = new int[]{numbs[i], numbs[k], numbs[c]};
+                                Arrays.sort(r1);
+                                found.put(r1[0] + "," + r1[1] + "," + r1[2], r1);
+                            }
+                        }
+                }
+            }
+        }
+
+        List<List<Integer>> r = new LinkedList<>();
+        for (int[] d: found.values()){
+            List<Integer> r1 = new LinkedList<>();
+            r1.add(d[0]); r1.add(d[1]); r1.add(d[2]);
+            r.add(r1);
+        }
+
+        return r;
+
+
+        /**
+         *
+         */
+
+    }
+
+
 
 
 }
